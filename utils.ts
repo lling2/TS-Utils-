7843e6a4b944f1ac444e3a702199f09cf59ca65b
lling2 <31692699+lling2@users.noreply.github.com>
2021-09-12 14:13:09 +0800

    Update utils.ts

--- utils.ts
+++ utils.ts
@@ -1,10 +1,10 @@
 function tuplify<T extends unknown[]>(...elements: T): T {
   return elements;
 }
-function get<T extends object, K extends keyof T>(o: T, name: K) {
+function getKey<T extends object, K extends keyof T>(o: T, name: K) {
   return o[name];
 }
 export { 
   tuplify, 
-  get 
+  getKey 
 };
